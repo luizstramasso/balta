9497ec26860ceb4e9ff5d3fd9ae6fe01af8710ea
luizstramasso <luizstramasso@gmail.com>
2026-05-26 16:29:14 -0300

    Add ICategoryHandler and ITransactionHandler interfaces for category and transaction management

---
+++ Fina/Fina.Core/Handlers/ITransactionHandler.cs
@@ -0,0 +1,14 @@
+using System.Transactions;
+using Fina.Core.Requests.Transactions;
+using Fina.Core.Responses;
+
+namespace Fina.Core.Handlers;
+
+public interface ITransactionHandler
+{
+    Task<Response<Transaction?>> CreateAsync(CreateTransactionRequest request);
+    Task<Response<Transaction?>> UpdateAsync(UpdateTransactionRequest request);
+    Task<Response<Transaction?>> DeleteAsync(DeleteTransactionRequest request);
+    Task<Response<Transaction?>> GetByIdAsync(GetTransactionByIdRequest request);
+    Task<Response<PagedResponse<List<Transaction>?>>> GetByPeriodAsync(GetTransactionsByPeriodRequest request);
+}
